b103503f4381d08616379bd48593ab0f58d10e13
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-03-05 21:20:20 +0300

    finished the challenge

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,32 @@
 'use strict';
 
-// Require our linked list implementation
 const LinkedLilst = require('../index');
+let linkedList = new LinkedLilst();
+let newLinkedList = new LinkedLilst();
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  it('Check if the linkedlist is empty', () => {
+    expect(linkedList.head).toEqual(null);
+  });
+  it('testing insert to an empty list', () => {
+    linkedList.insert(1);
+    expect(linkedList.head.value).toBe(1);
+    expect(linkedList.head.next).toBeNull();
+  });
+  it('testing insert to not empty linkedlist', () => {
+    linkedList.insert("A");
+    expect(linkedList.head.value).toBe("A");
+    expect(linkedList.head.next.value).toBe(1);
+    expect(linkedList.head.next.next).toBeNull();
+  });
+  it('testing includes fucntion', () => {
+    expect(linkedList.includes("A")).toBeTruthy();
+    expect(linkedList.includes(3)).toBeFalsy();
+    expect(newLinkedList.includes(3)).toBeFalsy();
+  });
+  it('testing toString function', () => {
+    expect(linkedList.toString()).toBe('{ A } -> { 1 } -> NULL');
   });
 });
+
+
